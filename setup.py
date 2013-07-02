57156b88f05df8b15bbeb0bede4a970ed2c277c2
Tom Martin <tmartin@codero.com>
2013-07-01 21:25:55 -0500

    added basic information

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 __author__ = 'Tom'
+
+from distutils.core import setup
+
+setup(
+    name='pomodoro',
+    version='0.1',
+    author='Tom Martin',
+    author_email='thomas.r.martin@gmail.com',
+    packages=['pomodoro', 'pomodoro.test'],
+    scripts=['bin/stowe-towels.py',],
+    url='http://pypi.python.org/pypi/pomodoro/',
+    license='LICENSE.txt',
+    description='Command line pomodoro',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        # "Django >= 1.1.1",
+        # "caldav == 0.1.4",
+    ],
+)
